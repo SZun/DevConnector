f582c95fe9be30116c3dccc2eef4a703d63afc0e
Sam G. Zun <zunsamuel97@gmail.com>
2018-08-12 04:59:13 -0500

    finished working on posts and comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,7 @@ import Spinner from '../common/Spinner';
 import { Link } from 'react-router-dom';
 import { getPost } from '../../actions/postActions';
 import CommentForm from './CommentForm';
+import CommentFeed from './CommentFeed';
 
 class Post extends Component {
   componentDidMount() {
@@ -22,6 +23,7 @@ class Post extends Component {
         <div>
           <PostItem post={post} showActions={false} />
           <CommentForm postId={post._id} />
+          <CommentFeed postId={post._id} comments={post.comments} />
         </div>
       );
     }
@@ -30,7 +32,7 @@ class Post extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <Link to="feed" className="btn btn-light mb-3">
+              <Link to="/feed" className="btn btn-light mb-3">
                 Back To Feed
               </Link>
               {postContent}
